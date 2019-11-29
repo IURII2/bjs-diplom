b54546b9fd00bd4a762a74de70eaaa0c15fa832e
IURII ARHMANN <iu.arhmann@gmail.com>
2019-11-29 16:41:10 +0300

    added code

--- static/main.js
+++ static/main.js
@@ -8,3 +8,133 @@ class Profile {
 		this.name = {firstName, lastName};
 		this.password = password;
 	}
+
+	createUser(callback) {
+		return ApiConnector.createUser(
+		{
+            username: this.username,
+            name: this.name,
+            password: this.password
+        },
+		(err, data) => {
+			console.log(`Creating user ${this.username}`);
+			callback(err, data);
+		});
+	}
+
+	performLogin(callback) {
+		let username = this.username;
+		let password = this.password;
+		return ApiConnector.performLogin({ username, password }, (err, data) => {
+			console.log(`Authorizing user ${username}`);
+			callback(err, data);
+		})
+	}
+
+	addMoney({ currency, amount }, callback) {
+		return ApiConnector.addMoney({ currency, amount }, (err, data) => {
+			console.log(`Adding ${amount} of ${currency} to ${this.username}`);
+			callback(err, data);
+		});
+	}
+
+	convertMoney({ fromCurrency, targetCurrency, targetAmount }, callback) {
+		return ApiConnector.convertMoney({ fromCurrency, targetCurrency, targetAmount }, (err, data) => {
+			console.log(`Converting ${fromCurrency} to ${targetAmount} ${targetCurrency}`);
+			callback(err, data);
+		});
+	}
+
+	transferMoney({ to, amount }, callback) {
+		return ApiConnector.transferMoney({ to, amount }, (err, data) => {
+			console.log(`Transfering ${amount} of Netcoins to ${to}`);
+			callback(err, data);
+		});
+	}
+}
+
+
+function getStocks(callback) {
+    return ApiConnector.getStocks((err, data) => {
+        console.log(`Getting stocks info`);
+        callback(err, data[99]);
+    });
+}
+
+
+function main() {
+	const ivan = new Profile({
+        username: 'ivan',
+        name: { firstName: 'Ivan', lastName: 'Chernyshev' },
+        password: 'ivanspass',
+    });
+
+	const petya = new Profile({
+		username: 'petya',
+		name: { firstName: 'Petya', lastName: 'Petrov'},
+		password: 'petyapass'
+	});
+
+	petya.createUser( (err, data) => {
+		if (err) {
+			console.log('Failed to create user');
+		} else {
+			console.log('Petya is created!');
+			petya.performLogin((err, data) => {
+				if (err) {
+					console.error('Failed to login');                
+				} else {
+					console.log('Petya is authorized');
+				}
+			});
+		}
+	});	
+	
+	getStocks((err, data) => {
+		if (err) {
+			console.error('Error during getting stocks');
+		}
+		const stocksInfo = data;
+		const wallet = { currency: 'EUR', amount: 500000 };
+	
+	ivan.createUser( (err, data) => {
+		if (err) {
+			console.log('Failed to create user');
+		} else {
+			console.log('Ivan is created!');
+			ivan.performLogin( (err, data) => {
+				if (err) {
+					console.log('Failed to login');
+				} else {
+					console.log('Ivan is authorized');
+					ivan.addMoney(wallet, (err, data) => {
+						if (err) {
+							console.log('Error during adding money to Ivan');
+						} else {
+							console.log('Added 500000 euros to Ivan');
+							ivan.convertMoney({ fromCurrency: 'EUR', targetCurrency: 'NETCOIN', targetAmount: 36000 }, (err, data) => {
+								if (err) {
+									console.log('Error during conversion');
+								} else {
+									const walletAmount = stocksInfo.NETCOIN_EUR * wallet.amount;
+									console.log('Converted to coins ', { name: {firstName: 'Ivan', lastName: 'Ivanov'}, wallet: {amount: walletAmount, currency: 'NETCOIN'}, username: 'ivan' });
+									ivan.transferMoney({ to: 'petya', amount: 36000 }, (err, data) => {
+										if (err) {
+											console.log('Failed to transfer 36000 Netcoins');
+										} else {
+											console.log('Petya has got 36000 Netcoins');
+										}
+									});
+								}
+							});
+						}
+					});
+				}
+		    });
+		}
+	});
+});
+
+}
+
+main();
